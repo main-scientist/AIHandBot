71759d9ac39c4b1596dbc811c773b1db34506604
Ivan Plotnikov <home@Ivans-MacBook-Pro.local>
2024-02-27 18:49:06 +0300

    wip

--- hand_bot.py
+++ hand_bot.py
@@ -52,85 +52,19 @@ def start_message(message):
             
         if report_eth is not None:
             wrapper_send_message(chat_id_ivan, json.dumps(report_eth, indent=2, separators=(',', ': ')), 0)
-        
-        
+                
         time.sleep(2)
-    
-    
-    
-    # while True:
-    #     report_btc, _date_btc, flag_btc = strategy_btc.strategy()
-        
-    #     # if flag_btc:
-    #     #     try:
-    #     #         bot.send_message(chat_id_ivan, f"Waiting BTC \n bank: {round(strategy_btc.bank, 2)} \n pred_position: {strategy_btc.pred_position}")
-    #     #         time.sleep(500)
-    #     #     except Exception as e:
-    #     #         print("Error from telegramm")
-    #     #         time.sleep(10)
-                
-    #     if _date_btc is False:
-    #         try:
-    #             bot.send_message(chat_id_ivan, "Error from bybit")
-    #         except Exception as e:
-    #             print("Error from telegramm")
-    #             time.sleep(1)
-    #         print("Error from bybit")
-    #     else:
-    #         if date_btc != _date_btc:
-    #             date_btc = _date_btc
-    #             try:    
-    #                 bot.send_message(chat_id_ivan, text=json.dumps(report_btc, indent=2, separators=(',', ': ')))
-    #                 bot.send_message(chat_id_ivan, text=f"start_bank_BTC: {100} \n now_bank_BTC: {round(report_btc['bank'], 2)} \n profit: {(round(report_btc['bank'], 2) - 100) / 100 * 100}")
-    #             except Exception as e:
-    #                 print("Error from telegramm")
-    #                 time.sleep(1)
-        
-        
-    #     report_sol, _date_sol, flag_sol = strategy_sol.strategy()
-        
-    #     if flag_sol:
-    #         try:
-    #             bot.send_message(chat_id_ivan, f"Waiting SOl \n bank: {round(strategy_sol.bank, 2)} \n pred_position: {strategy_sol.pred_position}")
-    #             time.sleep(500)
-    #         except Exception as e:
-    #             print("Error from telegramm")
-    #             time.sleep(10)
-                
-    #     if _date_sol is False:
-    #         try:
-    #             bot.send_message(chat_id_ivan, "Error from bybit")
-    #         except Exception as e:
-    #             print("Error from telegramm")
-    #             time.sleep(1)
-    #         print("Error from bybit")
-    #     else:
-    #         if date_sol != _date_sol:
-    #             date_sol = _date_sol
-    #             try:    
-    #                 bot.send_message(chat_id_ivan, text=json.dumps(report_sol, indent=2, separators=(',', ': ')))
-    #                 bot.send_message(chat_id_ivan, text=f"start_bank_SOL: {100} \n now_bank_SOL: {round(report_sol['bank'], 2)} \n profit: {(round(report_sol['bank'], 2) - 100) / 100 * 100}")
-    #             except Exception as e:
-    #                 print("Error from telegramm")
-    #                 time.sleep(1)
-                    
-    #     time.sleep(10)
-    
-    
+
         
 @bot.message_handler(commands=['button'])
 def button_message(message):
     markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
-    # item1=types.KeyboardButton("exit from SOl")
-    # item2=types.KeyboardButton("exit from BTC")
-    item3=types.KeyboardButton("exchange rate now")
-    item4=types.KeyboardButton("stop the bot")
-    item5=types.KeyboardButton("balance")
-    # markup.add(item1)
-    # markup.add(item2)
+    item1=types.KeyboardButton("exchange rate now")
+    item2=types.KeyboardButton("stop the bot")
+    item3=types.KeyboardButton("balance")
+    markup.add(item1)
+    markup.add(item2)
     markup.add(item3)
-    markup.add(item4)
-    markup.add(item5)
     bot.send_message(chat_id_ivan, text="choise action:", reply_markup=markup)
  
 
@@ -161,7 +95,12 @@ def message_reply(message):
         wrapper_send_message(chat_id_ivan, content, 0)
     
     if message.text=="balance":
-        content = f"bank_sol: {strategy_sol.bank} \n bank_btc: {strategy_btc.bank} \n bank_eth: {strategy_eth.bank}"
+        content = {
+            "bank_sol": round(strategy_sol.bank, 3),
+            "bank_btc": round(strategy_btc.bank, 3),
+            "bank_eth": round(strategy_eth.bank, 3)
+        }
+        content = json.dumps(content, indent=2, separators=(',', ': '))
         wrapper_send_message(chat_id_ivan, content, 0)
     
     # if message.text=="exit from SOl":
